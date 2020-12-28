95c22b76d0dbbc7ff90c211e16fdcbcc332e0fc5
Brendan Curry <Brendanstcurry@gmail.com>
2020-12-27 18:18:20 -0600

    update

--- src/com/bcurry/resizer/Resizer.java
+++ src/com/bcurry/resizer/Resizer.java
@@ -8,19 +8,24 @@ import java.util.concurrent.CountDownLatch;
 public class Resizer {
 	public static void main(String[] args) {
 		System.out.println("Starting...");
-		File inFile = new File("C:\\Users\\Redmancometh\\mmp\\abbyscraper\\titsjpg");
+		File inFile = new File(args[0]);
 		File[] files = inFile.listFiles();
 		List<File> fileList = Arrays.asList(files);
 		int chunkAmt = fileList.size() / 8;
 		CountDownLatch latch = new CountDownLatch(8);
 		System.out.println(chunkAmt);
 		for (int x = 1; x <= 8; x++) {
-			int start = (x * chunkAmt) - chunkAmt;
-			int end = x * chunkAmt;
-			System.out.println("Thread " + x + "Start: " + start);
-			System.out.println("Thread " + x + "End: " + end);
-			new ResizeThread(fileList.subList(start, end), latch, args[0], Integer.parseInt(args[1]),
-					Integer.parseInt(args[2])).start();
+			try {
+				int start = (x * chunkAmt) - chunkAmt;
+				int end = x * chunkAmt;
+				System.out.println("Thread " + x + "Start: " + start);
+				System.out.println("Thread " + x + "End: " + end);
+				new ResizeThread(fileList.subList(start, end), latch, args[1], Integer.parseInt(args[2]),
+						Integer.parseInt(args[3])).start();
+			} catch (Exception e) {
+				System.out.println("Syntax Error. Usage: java -jar [thisjarname].jar [inputDir] [outputDir] [width] [height]");
+				System.exit(0);
+			}
 		}
 		try {
 			latch.await();
